3e121be5bd9feb50253d8d3079141920cde5c326
Jeyadev L <74133351+Jeyadev42@users.noreply.github.com>
2024-12-02 22:37:51 +0530

    Update to use env variable

--- dags/dbt_pipeline.py
+++ dags/dbt_pipeline.py
@@ -23,20 +23,7 @@ with DAG(
 ) as dag:
 
     # Set the JSON content in an environment variable
-    service_account_key = """
-    {
-        "type": "service_account",
-        "project_id": "your-project-id",
-        "private_key_id": "your-private-key-id",
-        "private_key": "-----BEGIN PRIVATE KEY-----\\nYOUR-PRIVATE-KEY\\n-----END PRIVATE KEY-----\\n",
-        "client_email": "your-service-account-email",
-        "client_id": "your-client-id",
-        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-        "token_uri": "https://oauth2.googleapis.com/token",
-        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/your-service-account-email"
-    }
-    """
+    service_account_key = os.getenv("service_account_key")
 
     # Task 1: Write the JSON content to a temporary file
     write_service_account_key = BashOperator(
