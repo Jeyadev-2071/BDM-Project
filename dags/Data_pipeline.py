950f16d5e8557fc9f515afb61506074ecbfb345c
Jeyadev L <74133351+Jeyadev42@users.noreply.github.com>
2024-12-03 23:19:11 +0530

    test

--- dags/Data_pipeline.py
+++ dags/Data_pipeline.py
@@ -1,36 +1,36 @@
 from airflow import DAG
-from airflow.providers.google.cloud.operators.kubernetes_engine import GKEStartPodOperator
-from airflow.utils.dates import days_ago
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
 
 default_args = {
     'owner': 'JD',
-    'start_date': days_ago(1),
+    'start_date': datetime(2023, 1, 1),
     'retries': 1,
+    'retry_delay': timedelta(minutes=5),
 }
 
 with DAG(
-    'dbt_pipeline',
+    'update_dbt_docs',
     default_args=default_args,
-    description='Run Python script, dbt commands, and generate docs',
+    description='Run Python script and dbt commands in existing Kubernetes pod',
     schedule_interval='0 2 * * *',  # Adjust as needed
     catchup=False,
 ) as dag:
 
-    run_dbt_tasks = GKEStartPodOperator(
-        task_id='run_dbt_tasks',
-        project_id='iitj-capstone-project-group-18',
-        location='asia-south1',  # e.g., 'us-central1'
-        cluster_name='bdm-project',
-        namespace='default',
-        name='dbt-task-pod',
-        image='gcr.io/iitj-capstone-project-group-18/dbt_image:latest',
-        cmds=['/bin/bash', '-c'],
-        arguments=[
-            'python3 Python_Script/main.py && '
-            'dbt run && '
-            'dbt test && '
-            'dbt docs generate && '
-            'dbt docs serve --port 8080 --no-browser'
-        ],
-        get_logs=True,
+    run_python_script = BashOperator(
+        task_id='run_python_script',
+        bash_command=(
+            'kubectl exec -i -t deployment/dbt-deployement --'
+            'python3 Python_Scripts/main.py'
+        ),
     )
+
+    run_dbt_commands = BashOperator(
+        task_id='run_dbt_commands',
+        bash_command=(
+            'kubectl exec -i -t deployment/dbt-deployement --'
+            'bash -c "dbt run && dbt test && dbt docs generate"'
+        ),
+    )
+
+    run_python_script >> run_dbt_commands
